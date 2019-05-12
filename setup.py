6dc9c967c165979d433275822056178cf034a6dc
yuji <yuji38kwmt@yahoo.co.jp>
2019-05-12 22:28:33 +0900

    update api.mustache

--- setup.py
+++ setup.py
@@ -3,20 +3,29 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
 setup(name='annofabapi',
       version='0.0.1',
       description='Python Clinet Library of AnnoFab API (https://annofab.com/docs/api/)',
+      long_description=readme,
+      long_description_content_type='text/markdown',
       author='yuji38kwmt',
+      maintainer='yuji38kwmt',
       license='MIT',
       keywords='annofab api',
       url='https://github.com/kurusugawa-computer/annofab-api-python-client',
       install_requires=['requests', 'python-dateutil', 'backoff'],
+      python_requires='>=3.6',
       classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
             "License :: OSI Approved :: MIT License",
             "Programming Language :: Python",
             "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
             "Topic :: Utilities",
       ],
       packages=find_packages(exclude=["tests"])
