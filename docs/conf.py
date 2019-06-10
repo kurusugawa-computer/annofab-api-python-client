15ae9f9648b56852d1e0ff8c8a8203836bee216b
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-06-10 18:29:19 +0900

    readthedocに登録

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ release = '0.5.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon',
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'readthedocs_ext.readthedocs']
 
 # `__init__`のdocstringを出力する
 napoleon_include_init_with_doc = True
