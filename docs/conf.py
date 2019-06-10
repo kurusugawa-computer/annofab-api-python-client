37de5868e695170c6c6e024498c3145bee757e46
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-06-10 19:12:10 +0900

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,10 +30,9 @@ release = '0.5.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'readthedocs_ext.readthedocs']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
-# `__init__`のdocstringを出力する
-napoleon_include_init_with_doc = True
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
