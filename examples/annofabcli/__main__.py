f794c7ce03be54d500d26bb1c98d232f73d85a22
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-06-18 14:30:12 +0900

    examples: 一括差し戻しを追加

--- examples/annofabcli/__main__.py
+++ examples/annofabcli/__main__.py
@@ -7,22 +7,17 @@ import annofabcli.deprecated_complete_tasks
 import annofabcli.cancel_acceptance
 import annofabcli.diff_projects
 import annofabcli.invite_users
-
+import annofabcli.reject_tasks
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="annofabapiを使ったCLIツール", parents=[annofabcli.utils.create_parent_parser()], epilog="AnnoFab認証情報は`.netrc`に記載すること")
 
-    subparsers = parser.add_subparsers(title='subcommands',
-                                       description='valid subcommands',
-                                       help='additional help')
-
-
+    subparsers = parser.add_subparsers()
 
 
     annofabcli.deprecated_complete_tasks.parse_args(subparsers.add_parser("deprecated_complete_tasks",
         help="deprecated: タスクを受け入れ完了にする。その際、検査コメントを適切な状態にする。"))
-
 
     annofabcli.diff_projects.parse_args(subparsers.add_parser("diff_projects",
         help="プロジェクト間の差分を表示する。"
@@ -30,6 +25,9 @@ if __name__ == "__main__":
 
     annofabcli.invite_users.parse_args(subparsers.add_parser("invite_users",
         help="複数のプロジェクトに、ユーザを招待する。"))
+
+    annofabcli.reject_tasks.parse_args(subparsers.add_parser("reject_tasks",
+        help="検査コメントを付与してタスクを差し戻す。検査コメントは先頭の画像の左上(0,0)に付与する。"))
 
 
 
