1a8aa99cc7651aa4c81ae585e6c7708146e5011d
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-07-04 19:57:58 +0900

    update travis.yaml

--- annofabapi/__init__.py
+++ annofabapi/__init__.py
@@ -4,3 +4,17 @@ from annofabapi.wrapper import Wrapper
 from annofabapi.resource import build, build_from_netrc, build_from_env, Resource
 
 from .__version__ import __version__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
