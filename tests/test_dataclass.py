142ecf0f81eea980be3a6071be7ba8dda098b6f6
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-09-03 18:43:04 +0900

    format 修正

--- tests/test_dataclass.py
+++ tests/test_dataclass.py
@@ -44,38 +44,47 @@ organization_name = service.api.get_organization_of_project(project_id)[0]['orga
 annofab_user_id = service.api.login_user_id
 
 
-def test_annotation():
-    annotation_list = service.wrapper.get_all_annotation_list(project_id, query_params={'query': {'task_id': task_id}})
-    single_annotation = SingleAnnotation.from_dict(annotation_list[0])
-    assert type(single_annotation) == SingleAnnotation
+class TestAnnotation:
+    def test_simple_annotation(self):
+        annotation_list = service.wrapper.get_all_annotation_list(project_id,
+                                                                  query_params={'query': {
+                                                                      'task_id': task_id
+                                                                  }})
+        single_annotation = SingleAnnotation.from_dict(annotation_list[0])
+        assert type(single_annotation) == SingleAnnotation
 
-    dict_simple_annotation, _ = service.api.get_annotation(project_id, task_id, input_data_id)
-    simple_annotation = SimpleAnnotation.from_dict(dict_simple_annotation)
-    assert type(simple_annotation) == SimpleAnnotation
-
-
-def test_annotation_specs():
-    dict_annotation_specs, _ = service.api.get_annotation_specs(project_id)
-    annotation_specs = AnnotationSpecs.from_dict(dict_annotation_specs)
-    assert type(annotation_specs) == AnnotationSpecs
-
-
-def test_input():
-    input_data_list = service.wrapper.get_all_input_data_list(project_id, query_params={'task_id': task_id})
-    input_data = InputData.from_dict(input_data_list[0])
-    assert type(input_data) == InputData
+    def test_full_annotation(self):
+        dict_simple_annotation, _ = service.api.get_annotation(project_id, task_id, input_data_id)
+        simple_annotation = SimpleAnnotation.from_dict(dict_simple_annotation)
+        assert type(simple_annotation) == SimpleAnnotation
 
 
-def test_inspection():
-    inspection_list, _ = service.api.get_inspections(project_id, task_id, input_data_id)
-    inspection = Inspection.from_dict(inspection_list[0])
-    assert type(inspection) == Inspection
+class TestAnnotationSpecs:
+    def test_annotation_specs(self):
+        dict_annotation_specs, _ = service.api.get_annotation_specs(project_id)
+        annotation_specs = AnnotationSpecs.from_dict(dict_annotation_specs)
+        assert type(annotation_specs) == AnnotationSpecs
 
 
-def test_job():
-    job_list = service.wrapper.get_all_project_job(project_id, query_params={"type": "gen-tasks"})
-    job = JobInfo.from_dict(job_list[0])
-    assert type(job) == JobInfo
+class TestInput:
+    def test_input_data(self):
+        input_data_list = service.wrapper.get_all_input_data_list(project_id, query_params={'task_id': task_id})
+        input_data = InputData.from_dict(input_data_list[0])
+        assert type(input_data) == InputData
+
+
+class TestInspection:
+    def test_inspection(self):
+        inspection_list, _ = service.api.get_inspections(project_id, task_id, input_data_id)
+        inspection = Inspection.from_dict(inspection_list[0])
+        assert type(inspection) == Inspection
+
+
+class TestJob:
+    def test_job(self):
+        job_list = service.wrapper.get_all_project_job(project_id, query_params={"type": "gen-tasks"})
+        job = JobInfo.from_dict(job_list[0])
+        assert type(job) == JobInfo
 
 
 class TestMy:
@@ -90,87 +99,92 @@ class TestMy:
         assert type(my_account) == MyAccount
 
 
-def test_organization():
-    dict_organization, _ = service.api.get_organization(organization_name)
-    organization = Organization.from_dict(dict_organization)
-    assert type(organization) == Organization
+class TestOrganization:
+    def test_organization(self):
+        dict_organization, _ = service.api.get_organization(organization_name)
+        organization = Organization.from_dict(dict_organization)
+        assert type(organization) == Organization
 
-    dict_organization_activity, _ = service.api.get_organization_activity(organization_name)
-    organization_activity = OrganizationActivity.from_dict(dict_organization_activity)
-    assert type(organization_activity) == OrganizationActivity
-
-
-def test_organization_member():
-    dict_organization_member, _ = service.api.get_organization_member(organization_name, annofab_user_id)
-    organization_member = OrganizationMember.from_dict(dict_organization_member)
-    assert type(organization_member) == OrganizationMember
+    def test_organization_activity(self):
+        dict_organization_activity, _ = service.api.get_organization_activity(organization_name)
+        organization_activity = OrganizationActivity.from_dict(dict_organization_activity)
+        assert type(organization_activity) == OrganizationActivity
 
 
-def test_project():
-    dict_project, _ = service.api.get_project(project_id)
-    project = Project.from_dict(dict_project)
-    assert type(project) == Project
+class TestOrganizationMember:
+    def test_organization_member(self):
+        dict_organization_member, _ = service.api.get_organization_member(organization_name, annofab_user_id)
+        organization_member = OrganizationMember.from_dict(dict_organization_member)
+        assert type(organization_member) == OrganizationMember
 
 
-def test_project_member():
-    dict_project_member, _ = service.api.get_project_member(project_id, annofab_user_id)
-    project_member = ProjectMember.from_dict(dict_project_member)
-    assert type(project_member) == ProjectMember
+class TestProject:
+    def test_project(self):
+        dict_project, _ = service.api.get_project(project_id)
+        project = Project.from_dict(dict_project)
+        assert type(project) == Project
 
 
-def test_statistics_get_task_statistics():
-    stat_list, _ = service.api.get_task_statistics(project_id)
-    stat = ProjectTaskStatisticsHistory.from_dict(stat_list[0])
-    assert type(stat) == ProjectTaskStatisticsHistory
+class TestProjectMember:
+    def test_project_member(self):
+        dict_project_member, _ = service.api.get_project_member(project_id, annofab_user_id)
+        project_member = ProjectMember.from_dict(dict_project_member)
+        assert type(project_member) == ProjectMember
 
 
-def test_statistics_get_account_statistics():
-    stat_list, _ = service.api.get_account_statistics(project_id)
-    stat = ProjectAccountStatistics.from_dict(stat_list[0])
-    assert type(stat) == ProjectAccountStatistics
+class TestStatistics:
+    def test_statistics_get_task_statistics(self):
+        stat_list, _ = service.api.get_task_statistics(project_id)
+        stat = ProjectTaskStatisticsHistory.from_dict(stat_list[0])
+        assert type(stat) == ProjectTaskStatisticsHistory
+
+    def test_statistics_get_account_statistics(self):
+        stat_list, _ = service.api.get_account_statistics(project_id)
+        stat = ProjectAccountStatistics.from_dict(stat_list[0])
+        assert type(stat) == ProjectAccountStatistics
+
+    def test_statistics_get_inspection_statistics(self):
+        stat_list, _ = service.api.get_inspection_statistics(project_id)
+        stat = InspectionStatistics.from_dict(stat_list[0])
+        assert type(stat) == InspectionStatistics
+
+    def test_statistics_get_task_phase_statistics(self):
+        stat_list, _ = service.api.get_task_phase_statistics(project_id)
+        stat = TaskPhaseStatistics.from_dict(stat_list[0])
+        assert type(stat) == TaskPhaseStatistics
+
+    def test_statistics_get_label_statistics(self):
+        stat_list, _ = service.api.get_label_statistics(project_id)
+        stat = LabelStatistics.from_dict(stat_list[0])
+        assert type(stat) == LabelStatistics
+
+    def test_statistics_get_worktime_statistics(self):
+        stat_list = service.wrapper.get_worktime_statistics(project_id)
+        stat = WorktimeStatistics.from_dict(stat_list[0])
+        assert type(stat) == WorktimeStatistics
 
 
-def test_statistics_get_inspection_statistics():
-    stat_list, _ = service.api.get_inspection_statistics(project_id)
-    stat = InspectionStatistics.from_dict(stat_list[0])
-    assert type(stat) == InspectionStatistics
+class TestSupplementary:
+    def test_supplementary(self):
+        supplementary_data_list, _ = service.api.get_supplementary_data_list(project_id, input_data_id)
+        supplementary_data = SupplementaryData.from_dict(supplementary_data_list[0])
+        assert type(supplementary_data) == SupplementaryData
 
 
-def test_statistics_get_task_phase_statistics():
-    stat_list, _ = service.api.get_task_phase_statistics(project_id)
-    stat = TaskPhaseStatistics.from_dict(stat_list[0])
-    assert type(stat) == TaskPhaseStatistics
+class TestTask:
+    def test_task(self):
+        dict_task, _ = service.api.get_task(project_id, task_id)
+        task = Task.from_dict(dict_task)
+        assert type(task) == Task
+
+    def test_task_history(self):
+        task_histories, _ = service.api.get_task_histories(project_id, task_id)
+        task_history = TaskHistory.from_dict(task_histories[0])
+        assert type(task_history) == TaskHistory
 
 
-def test_statistics_get_label_statistics():
-    stat_list, _ = service.api.get_label_statistics(project_id)
-    stat = LabelStatistics.from_dict(stat_list[0])
-    assert type(stat) == LabelStatistics
-
-
-def test_statistics_get_worktime_statistics():
-    stat_list = service.wrapper.get_worktime_statistics(project_id)
-    stat = WorktimeStatistics.from_dict(stat_list[0])
-    assert type(stat) == WorktimeStatistics
-
-
-def test_supplementary():
-    supplementary_data_list, _ = service.api.get_supplementary_data_list(project_id, input_data_id)
-    supplementary_data = SupplementaryData.from_dict(supplementary_data_list[0])
-    assert type(supplementary_data) == SupplementaryData
-
-
-def test_task():
-    dict_task, _ = service.api.get_task(project_id, task_id)
-    task = Task.from_dict(dict_task)
-    assert type(task) == Task
-
-    task_histories, _ = service.api.get_task_histories(project_id, task_id)
-    task_history = TaskHistory.from_dict(task_histories[0])
-    assert type(task_history) == TaskHistory
-
-
-def test_webhook():
-    webhook_list = service.api.get_webhooks(project_id)[0]
-    webhook = Webhook.from_dict(webhook_list[0])
-    assert type(webhook) == Webhook
+class TestWebhook:
+    def test_webhook(self):
+        webhook_list = service.api.get_webhooks(project_id)[0]
+        webhook = Webhook.from_dict(webhook_list[0])
+        assert type(webhook) == Webhook
