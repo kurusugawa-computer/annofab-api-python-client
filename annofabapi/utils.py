0edd72d837a63324d80df865e5bde81a86e30e76
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-10-17 14:55:01 +0900

    update logger

--- annofabapi/utils.py
+++ annofabapi/utils.py
@@ -44,10 +44,10 @@ def log_error_response(arg_logger: logging.Logger, response: requests.Response):
     """
 
     if 400 <= response.status_code < 600:
-        arg_logger.debug(f"status_code = {response.status_code}, response.text = {response.text}")
-        arg_logger.debug(f"request.url = {response.request.method}  {response.request.url}")
-        arg_logger.debug(f"request.headers = {response.request.headers}")
-        arg_logger.debug(f"request.body = {response.request.body}")
+        arg_logger.debug(f"status_code = %s, response.text = %s", response.status_code, response.text)
+        arg_logger.debug(f"request.url = %s %s", response.request.method, response.request.url)
+        arg_logger.debug("request.headers = %s", response.request.headers)
+        arg_logger.debug("request.body = %s", response.request.body)
 
 
 def download(url: str, dest_path: str):
--- annofabapi/wrapper.py
+++ annofabapi/wrapper.py
@@ -562,10 +562,9 @@ class Wrapper:
             updated_project_members.append(updated_project_member)
 
             command_name = '追加' if last_updated_datetime is None else '更新'
-            logger.debug(
-                "プロジェクトメンバの'%s' 完了."
-                " project_id=%s, user_id=%s, "
-                "last_updated_datetime=%s", command_name, project_id, member['user_id'], last_updated_datetime)
+            logger.debug("プロジェクトメンバの'%s' 完了."
+                         " project_id=%s, user_id=%s, "
+                         "last_updated_datetime=%s", command_name, project_id, member['user_id'], last_updated_datetime)
 
         return updated_project_members
 
