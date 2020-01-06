18da54dd1b09949f1de16784f565124fb8c5b481
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-01-06 14:29:59 +0900

    generated_api.py, models.pyを更新

--- annofabapi/generated_api2.py
+++ annofabapi/generated_api2.py
@@ -45,9 +45,10 @@ class AbstractAnnofabApi2(abc.ABC):
             query_params (Dict[str, Any]): Query Parameters
                 cache (str):  CACHE TIMESTAMP 
                 history_id (str):  過去のアノテーション仕様を取得する場合、[アノテーション仕様履歴取得](#operation/getAnnotationSpecsHistories)APIで取得した `history_id` の値を指定します。 未指定時は最新のアノテーション仕様を取得します。 
+                v (str):  取得するアノテーション仕様のフォーマットバージョンを指定します。 
 
         Returns:
-            Tuple[AnnotationSpecs, requests.Response]
+            Tuple[OneOfAnnotationSpecsV1AnnotationSpecsV2, requests.Response]
 
 
         """
