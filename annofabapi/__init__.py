b6af25d2d747fdd961069d2ac43bac1abcbf4bcf
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-04-29 12:40:48 +0900

    flake8対応："__all__"を使って "import but unused"警告が出ないようにする https://qiita.com/junkoda/items/2abb237d4d6b5a85989e

--- annofabapi/__init__.py
+++ annofabapi/__init__.py
@@ -4,3 +4,14 @@ from annofabapi.wrapper import Wrapper
 from annofabapi.resource import build, build_from_netrc, build_from_env, Resource
 
 from .__version__ import __version__
+
+__all__ = [
+    "AnnofabApi",
+    "AnnofabApi2",
+    "Wrapper",
+    "build",
+    "build_from_netrc",
+    "build_from_env",
+    "Resource",
+    "__version__",
+]
