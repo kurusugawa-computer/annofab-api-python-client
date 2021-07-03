d5ee52da8fb54406cecfdde8faedcdc6ffb78799
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-07-04 00:10:13 +0900

    get_all_project_jobのquery_paramsは必須でないので、デフォルト値をNoneにする

--- annofabapi/wrapper.py
+++ annofabapi/wrapper.py
@@ -1871,7 +1871,7 @@ class Wrapper:
 
         return deleted_jobs
 
-    def get_all_project_job(self, project_id: str, query_params: Dict[str, Any]) -> List[ProjectJobInfo]:
+    def get_all_project_job(self, project_id: str, query_params: Dict[str, Any] = None) -> List[ProjectJobInfo]:
         """
         すべてのバックグランドジョブを取得する。
 
