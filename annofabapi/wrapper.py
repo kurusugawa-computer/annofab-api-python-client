4de09037e08cf2e52d08f8048a3ac2c1165b80ed
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-10-26 14:58:14 +0900

    private ->protected

--- annofabapi/wrapper.py
+++ annofabapi/wrapper.py
@@ -370,7 +370,7 @@ class Wrapper:
 
         return dest_detail
 
-    def __create_request_body_for_copy_annotation(
+    def _create_request_body_for_copy_annotation(
         self,
         project_id: str,
         task_id: str,
@@ -1476,6 +1476,21 @@ class Wrapper:
             タスク
         """
         content, _ = self.api.get_task(project_id, task_id)
+        return content
+
+    @allow_404_error
+    def get_task_histories_or_none(self, project_id: str, task_id: str) -> Optional[Task]:
+        """
+        タスク履歴一覧を取得する。存在しない場合(HTTP 404 Error)はNoneを返す。
+
+        Args:
+            project_id:
+            task_id:
+
+        Returns:
+            タスク履歴一覧
+        """
+        content, _ = self.api.get_task_histories(project_id, task_id)
         return content
 
     def get_all_tasks(self, project_id: str, query_params: Optional[Dict[str, Any]] = None) -> List[Task]:
