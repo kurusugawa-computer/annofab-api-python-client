3019fcd875f7608c92d76a3edcae1ecbe785733c
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-01-05 14:02:32 +0900

    ログメッセージの変更

--- annofabapi/api.py
+++ annofabapi/api.py
@@ -348,23 +348,28 @@ class AnnofabApi(AbstractAnnofabApi):
             requests.exceptions.HTTPError: http status codeが4XXX,5XXXのとき
 
         """
-
-        logger.debug(
-            "Sending a request :: %s",
-            {
-                "http_method": http_method,
-                "url": url,
-                "query_params": params,
-                "request_body_json": _create_request_body_for_logger(json),
-                "request_body_data": _create_request_body_for_logger(data),
-                "header_params": headers,
-            },
-        )
-
         response = self.session.request(
             method=http_method, url=url, params=params, data=data, headers=headers, json=json, **kwargs
         )
 
+        # response.requestよりメソッド引数のrequest情報の方が分かりやすいので、メソッド引数のrequest情報を出力する。
+        logger.debug(
+            "Sent a request :: %s",
+            {
+                "requests": {
+                    "http_method": http_method,
+                    "url": url,
+                    "query_params": params,
+                    "request_body_json": _create_request_body_for_logger(json) if json is not None else None,
+                    "request_body_data": _create_request_body_for_logger(data) if data is not None else None,
+                    "header_params": headers,
+                },
+                "response": {
+                    "status_code": response.status_code,
+                    "content_length": len(response.content),
+                },
+            },
+        )
         # リトライすべき場合はExceptionを返す
         if raise_for_status or _should_retry_with_status(response.status_code):
             _log_error_response(logger, response)
@@ -408,19 +413,24 @@ class AnnofabApi(AbstractAnnofabApi):
             url = f"{self.url_prefix}{url_path}"
 
         kwargs = self._create_kwargs(query_params, header_params, request_body)
-
-        # HTTP Requestを投げる
+        response = self.session.request(method=http_method.lower(), url=url, **kwargs)
+        # response.requestよりメソッド引数のrequest情報の方が分かりやすいので、メソッド引数のrequest情報を出力する。
         logger.debug(
-            "Sending a request :: %s",
+            "Sent a request :: %s",
             {
-                "http_method": http_method.lower(),
-                "url": url,
-                "query_params": query_params,
-                "header_params": header_params,
-                "request_body": _create_request_body_for_logger(request_body) if request_body is not None else None,
+                "request": {
+                    "http_method": http_method.lower(),
+                    "url": url,
+                    "query_params": query_params,
+                    "header_params": header_params,
+                    "request_body": _create_request_body_for_logger(request_body) if request_body is not None else None,
+                },
+                "response": {
+                    "status_code": response.status_code,
+                    "content_length": len(response.content),
+                },
             },
         )
-        response = self.session.request(method=http_method.lower(), url=url, **kwargs)
 
         # Unauthorized Errorならば、ログイン後に再度実行する
         if response.status_code == requests.codes.unauthorized:
