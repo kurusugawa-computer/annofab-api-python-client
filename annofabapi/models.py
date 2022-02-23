100083ac0cfc533681b92705515e078e05e52fd9
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-02-23 14:03:01 +0900

    update models.py

--- annofabapi/models.py
+++ annofabapi/models.py
@@ -2110,16 +2110,6 @@ Kyes of Dict
 """
 
 
-class InspectionStatus(Enum):
-    """
-    ##### スレッドの先頭のコメントである（`parent_inspection_id` に値がない）場合  * `annotator_action_required` - 未処置。`annotation`フェーズ担当者が何らかの回答をする必要あり * `no_correction_required` - 処置不要。`annotation`フェーズ担当者が、検査コメントによる修正は不要、と回答した * `error_corrected` - 修正済み。`annotation`フェーズ担当者が、検査コメントの指示どおり修正した  ##### 返信コメントである（`parent_inspection_id` に値がある）場合  現在は使用しておらず、レスポンスに含まれる値は不定です。APIのレスポンスにこの値を含む場合でも、「スレッドの先頭のコメント」の値を利用してください。  リクエストボディに指定する場合は、スレッドの先頭のコメントと同じ値を指定します。
-    """
-
-    ANNOTATOR_ACTION_REQUIRED = "annotator_action_required"
-    NO_CORRECTION_REQUIRED = "no_correction_required"
-    ERROR_CORRECTED = "error_corrected"
-
-
 class InspectionSummary(Enum):
     """
     - `no_inspection` - 入力データに検査コメントが付けられていない。 - `no_comment_inspection` - 入力データに空の検査コメントが付けられている。 - `new_reply_to_unprocessed` - 現在進行中の検査・受入フェーズで未処理の検査コメントに対して新たに返信が付けられている。 - `new_unprocessed_inspection` - 現在進行中の検査・受入フェーズでつけられた検査コメントのうち、未処理のものが1つ以上ある。 - `unprocessed` - 過去の検査・受入フェーズでつけられた検査コメントのうち、未処理のものが1つ以上ある。 - `complete` - 入力データにつけられた検査コメントで未処理のものがない。
@@ -4760,3 +4750,14 @@ Kyes of Dict
     作業時間の標準偏差（ISO 8601 duration）
 
 """
+
+
+@deprecated_class(deprecated_date="2022-08-23")
+class InspectionStatus(Enum):
+    """
+    ##### スレッドの先頭のコメントである（`parent_inspection_id` に値がない）場合  * `annotator_action_required` - 未処置。`annotation`フェーズ担当者が何らかの回答をする必要あり * `no_correction_required` - 処置不要。`annotation`フェーズ担当者が、検査コメントによる修正は不要、と回答した * `error_corrected` - 修正済み。`annotation`フェーズ担当者が、検査コメントの指示どおり修正した  ##### 返信コメントである（`parent_inspection_id` に値がある）場合  現在は使用しておらず、レスポンスに含まれる値は不定です。APIのレスポンスにこの値を含む場合でも、「スレッドの先頭のコメント」の値を利用してください。  リクエストボディに指定する場合は、スレッドの先頭のコメントと同じ値を指定します。
+    """
+
+    ANNOTATOR_ACTION_REQUIRED = "annotator_action_required"
+    NO_CORRECTION_REQUIRED = "no_correction_required"
+    ERROR_CORRECTED = "error_corrected"
