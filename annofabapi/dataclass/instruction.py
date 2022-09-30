7776d3526f47e654763e68286d009e84ec23dc6c
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-09-30 13:50:45 +0900

    instructionも不要にする

--- annofabapi/dataclass/instruction.py
+++ annofabapi/dataclass/instruction.py
@@ -15,6 +15,8 @@ from typing import Any, Dict, List, NewType, Optional, Tuple, Union  # pylint: d
 
 from dataclasses_json import DataClassJsonMixin
 
+warnings.warn("'annofabapi.dataclass.instruction'モジュールは2022-12-01以降に廃止する予定です。", FutureWarning, stacklevel=2)
+
 
 @dataclass
 class Instruction(DataClassJsonMixin):
