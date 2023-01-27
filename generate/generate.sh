ebfb7e49f08b0da66a35cf369cd06410868e7595
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2023-01-27 17:09:31 +0900

    update sh

--- generate/generate.sh
+++ generate/generate.sh
@@ -126,12 +126,12 @@ declare -a model_files=(${MODELS_DIR}/point.py \
  ${MODELS_DIR}/full_annotation.py \
  ${MODELS_DIR}/simple_annotation_detail.py \
  ${MODELS_DIR}/simple_annotation.py \
+ ${MODELS_DIR}/additional_data_v2.py \
  ${MODELS_DIR}/single_annotation_detail_v1.py \
  ${MODELS_DIR}/single_annotation_detail_v2.py \
  ${MODELS_DIR}/single_annotation.py \
  ${MODELS_DIR}/annotation_detail_v1.py \
  ${MODELS_DIR}/annotation_v1.py \
- ${MODELS_DIR}/additional_data_v2.py \
  ${MODELS_DIR}/annotation_props_for_editor.py \
  ${MODELS_DIR}/additional_data_value_flag.py \
  ${MODELS_DIR}/additional_data_value_integer.py \
