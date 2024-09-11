c19f62e3b1d467199aeda65a48a90d3c5f4598dc
Toshiya MORI <mori@kurusugawa.jp>
2024-09-11 21:22:01 +0900

    正しくないコメントを修正

--- annofabapi/util/type_util.py
+++ annofabapi/util/type_util.py
@@ -3,4 +3,4 @@ from typing import NoReturn
 
 def assert_noreturn(x: NoReturn) -> NoReturn:
     """python 3.11 以降に追加されたassert_neverの代わり"""
-    raise AssertionError(f"Invalid value: {x!r}")  # x!rは str(x)と等価
+    raise AssertionError(f"Invalid value: {x!r}")  # x!rは repr(x)と等価
