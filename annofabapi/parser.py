8f2e7840857001921554ccc17f3855d6ebd888d4
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2025-01-06 16:06:26 +0900

    update ruff

--- annofabapi/parser.py
+++ annofabapi/parser.py
@@ -240,7 +240,7 @@ class SimpleAnnotationDirParser(SimpleAnnotationParser):
     def open_outer_file(self, data_uri: str):  # noqa: ANN201
         outer_file_path = _trim_extension(self.json_file_path) + "/" + data_uri
         try:
-            return open(outer_file_path, mode="rb")  # noqa: SIM115, pylint: disable=consider-using-with
+            return open(outer_file_path, mode="rb")
         except FileNotFoundError as e:
             raise AnnotationOuterFileNotFoundError(str(outer_file_path)) from e
 
@@ -306,7 +306,7 @@ class FullAnnotationDirParser(FullAnnotationParser):
     def open_outer_file(self, data_uri: str):  # noqa: ANN201
         outer_file_path = _trim_extension(self.json_file_path) + "/" + data_uri
         try:
-            return open(outer_file_path, mode="rb")  # noqa: SIM115, pylint: disable=consider-using-with
+            return open(outer_file_path, mode="rb")
         except FileNotFoundError as e:
             raise AnnotationOuterFileNotFoundError(str(outer_file_path)) from e
 
