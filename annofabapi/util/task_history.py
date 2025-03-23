26411beceb996d86669261b03fef6e0a89b80eec
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2025-03-23 16:30:11 +0900

    テストコードの追加

--- annofabapi/util/task_history.py
+++ annofabapi/util/task_history.py
@@ -16,18 +16,16 @@ def find_rejected_task_history_indices(task_history_list: list[dict[str, Any]]) 
         差し戻されたタスク履歴のインデックス番号のリスト
     """
     index_list = []
-    print(f"adsafr")
     for index, history in enumerate(task_history_list):
         # 検査/受入フェーズで作業が行われているか
         if not (
             history["phase"] in {TaskPhase.INSPECTION.value, TaskPhase.ACCEPTANCE.value}
-            and history["account_id"] is None
             and isodate.parse_duration(history["accumulated_labor_time_milliseconds"]).total_seconds() > 0
+            and history["account_id"] is not None
             and history["started_datetime"] is not None
             and history["ended_datetime"] is not None
         ):
             continue
-        print(f"{history=}, {index=}")
         # 直後の履歴が教師付フェーズならば、差し戻されたとみなす
         next_index = index + 1
         if next_index >= len(task_history_list):
